860818ff955aafbb4cc3ee85da897d974a7250d9
maclover7 <me@jonathanmoss.me>
2015-08-07 21:50:42 -0400

    ability to add karma to users

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ source 'http://rubygems.org'
 gem 'slack-ruby-bot'
 gem 'puma'
 gem 'sinatra'
+gem 'redis'
+
+group :development, :test do
+  gem 'pry'
+end
--- bot.rb
+++ bot.rb
@@ -1,6 +1,13 @@
 require 'slack-ruby-bot'
 
+if ENV["RACK_ENV"] != "production"
+  require 'pry'
+end
+
 module Bot
+
+  REDIS_CONN = Redis.new(:url => ENV["REDISTOGO_URL"] || "redis://localhost:6379")
+
   class App < SlackRubyBot::App
   end
 
@@ -15,5 +22,17 @@ module Bot
       keyword = _match[1]
       send_message_with_gif(client, data.channel, "", keyword)
     end
+  end
+
+  class Karma < SlackRubyBot::Commands::Base
+    # Add karma
+    match /(\S+[^+:\s])[: ]*\+\+(\s|$)/ do |client, data, _match|
+      user = _match[1]
+      REDIS_CONN.incr("#{user}_karma")
+      karma_count = REDIS_CONN.get("#{user}_karma")
+      client.message text: "#{user} is on the rise! (Karma: #{karma_count})", channel: data.channel
+    end
+
+    # Remove karma
   end
 end
